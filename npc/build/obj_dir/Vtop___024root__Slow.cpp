4501fc9aa35c50ce135b5f081274489e1ad3f53a
22041098-Man Zeyuan <abcs.ml@hotmail.com>
2022-04-10 19:15:38 -0700

    led to nvboard

---
+++ npc/build/obj_dir/Vtop___024root__Slow.cpp
@@ -0,0 +1,75 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtop.h for the primary calling header
+
+#include "Vtop___024root.h"
+#include "Vtop__Syms.h"
+
+//==========
+
+
+void Vtop___024root___ctor_var_reset(Vtop___024root* vlSelf);
+
+Vtop___024root::Vtop___024root(const char* _vcname__)
+    : VerilatedModule(_vcname__)
+ {
+    // Reset structure values
+    Vtop___024root___ctor_var_reset(this);
+}
+
+void Vtop___024root::__Vconfigure(Vtop__Syms* _vlSymsp, bool first) {
+    if (false && first) {}  // Prevent unused
+    this->vlSymsp = _vlSymsp;
+}
+
+Vtop___024root::~Vtop___024root() {
+}
+
+void Vtop___024root___eval_initial(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_initial\n"); );
+    // Body
+    vlSelf->__Vclklast__TOP__clk = vlSelf->clk;
+}
+
+void Vtop___024root___eval_settle(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_settle\n"); );
+}
+
+void Vtop___024root___final(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___final\n"); );
+}
+
+void Vtop___024root___ctor_var_reset(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___ctor_var_reset\n"); );
+    // Body
+    vlSelf->clk = 0;
+    vlSelf->rst = 0;
+    vlSelf->sw = 0;
+    vlSelf->ps2_clk = 0;
+    vlSelf->ps2_data = 0;
+    vlSelf->ledr = 0;
+    vlSelf->VGA_CLK = 0;
+    vlSelf->VGA_HSYNC = 0;
+    vlSelf->VGA_VSYNC = 0;
+    vlSelf->VGA_BLANK_N = 0;
+    vlSelf->VGA_R = 0;
+    vlSelf->VGA_G = 0;
+    vlSelf->VGA_B = 0;
+    vlSelf->seg0 = 0;
+    vlSelf->seg1 = 0;
+    vlSelf->seg2 = 0;
+    vlSelf->seg3 = 0;
+    vlSelf->seg4 = 0;
+    vlSelf->seg5 = 0;
+    vlSelf->seg6 = 0;
+    vlSelf->seg7 = 0;
+    vlSelf->top__DOT__led1__DOT__count = 0;
+}
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,32 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "Vexample.h"
 
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
+#include <nvboard.h>
+#include <Vtop.h>
+
 #define MAX_SIM_TIME 20
 vluint64_t sim_time = 0;
 
+static TOP_NAME top;
+// Vtop *top = new Vtop;
+// VerilatedVcdC *tfp = new VerilatedVcdC;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+void single_cycle() {
+  top.clk = 0; top.eval();
+  top.clk = 1; top.eval();
+}
+
+void reset(int n) {
+  top.rst = 1;
+  while (n -- > 0) single_cycle();
+  top.rst = 0;
+}
+
 int main(){
-  Verilated::traceEverOn(true);
-  Vexample *top = new Vexample;
-  VerilatedVcdC *tfp = new VerilatedVcdC;
-  top->trace(tfp,10);
-  tfp->open("test.vcd");
+  // Verilated::traceEverOn(true);
+  // top->trace(tfp,10);
+  // tfp->open("light.vcd");
 
-  while (sim_time < MAX_SIM_TIME) {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == a ^ b);
+  nvboard_bind_all_pins(&top);
+  nvboard_init();
 
-    tfp->dump(sim_time);
-    sim_time++;
+  reset(10);
+  while (true) {
+    single_cycle();
+
+    // tfp->dump(sim_time);
+    // sim_time++;
+    nvboard_update();
   }
-  tfp->close();
+  nvboard_quit();
+  // tfp->close();
 }
