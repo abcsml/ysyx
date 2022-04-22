22fad0d1f9e17f74046ba23e3d2275162658d7ef
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-22 15:02:37 +0800

    > compile NEMU ysyx_22041098 满泽远 Linux ad-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:02:37 up 14:28,  1 user,  load average: 0.22, 0.25, 0.22

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -173,7 +173,7 @@ static uint32_t eval(int p, int q, bool *success) {
         return 0;
       }
     }
-    printf("op:%c\n", tokens[op].type);
+    printf("%d op:%c\n", op, tokens[op].type);
 
     uint32_t val1 = eval(p, op - 1, success);
     uint32_t val2 = eval(op + 1, q, success);
