a27b09c7acb4edea1c08eeb24b215c80f0914b0a
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-25 14:28:27 +0800

    > compile NEMU ysyx_22041098 满泽远 Linux ad-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:28:27 up 1 day,  1:02,  1 user,  load average: 0.03, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -87,8 +87,8 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        //     i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
 
@@ -102,6 +102,8 @@ static bool make_token(char *e) {
           default:
             tokens[nr_token].type = rules[i].token_type;
             strncpy(tokens[nr_token].str, substr_start, substr_len);
+            Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+            i, rules[i].regex, position, substr_len, substr_len, substr_start);
             nr_token++;
         }
 
