ee99a6cca3012a27a4cb47891d8c4c0f6232236a
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-12 11:34:25 +0800

    > compile NEMU ysyx_22041098 满泽远 Linux ad-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:34:25 up 1 day,  9:02,  1 user,  load average: 0.15, 0.11, 0.04

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -6,10 +6,7 @@
 
 size_t strlen(const char *s) {
   size_t len = 0;
-  while (*s != '\0') {
-    len++;
-    s++;
-  }
+  for (len = 0; s[len] != '\0'; len++);
   return len;
 }
 
@@ -31,31 +28,78 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t dst_len = strlen(dst);
+  size_t i;
+  for (i = 0 ; src[i] != '\0' ; i++)
+    dst[dst_len + i] = src[i];
+  dst[dst_len + i] = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; s1[i] != '\0'; i++) {
+    if (s1[i] != s2[i]) {
+      if (s1[i] < s2[i])
+        return -1;
+      else
+        return 1;
+    }
+  }
+  if (s2[i] != '\0')
+    return -1;
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; i < n && s1[i] != '\0'; i++) {
+    if (s1[i] != s2[i]) {
+      if (s1[i] < s2[i])
+        return -1;
+      else
+        return 1;
+    }
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  if (s == NULL || n == 0)
+    return NULL;
+  uint8_t *pS = s;
+
+  for (size_t i = 0; i < n; i++) {
+    pS[i] = (uint8_t) c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  if (dst == NULL || src == NULL || n == 0)
+    return NULL;
+  uint8_t *pDst = dst;
+  const uint8_t *pSrc = src;
+
+  for (size_t i = 0; i < n; i++)
+    pDst[i] = pSrc[i];
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  if (out == NULL || in == NULL || n == 0)
+    return NULL;
+  uint8_t *pOut = out;
+  const uint8_t *pIn = in;
+
+  for (size_t i = 0; i < n; i++)
+    pOut[i] = pIn[i];
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  // if (s1 == NULL || s2 == NULL)
+    return 0;
 }
 
 #endif
