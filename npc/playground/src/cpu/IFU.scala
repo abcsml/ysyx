af89ceefd3a8542adc53ae1e7d940afee4e0ae79
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-18 10:46:59 +0800

    > generate verilog ysyx_22041098 满泽远 Linux ad-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:46:59 up 2 days,  6:53,  1 user,  load average: 0.44, 0.35, 0.25

--- npc/playground/src/cpu/IFU.scala
+++ npc/playground/src/cpu/IFU.scala
@@ -10,7 +10,7 @@ class MEM extends BlackBox with HasBlackBoxPath {
     val len  = Input(UInt(3.W))
     val data = Output(UInt(32.W))
   })
-  addPath("../csrc/mem.v")
+  addPath("src/csrc/mem.v")
 }
 
 class IFU extends Module {
