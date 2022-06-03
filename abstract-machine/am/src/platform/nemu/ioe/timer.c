e9560108854ef4edcf847651045aa53a2250a00c
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-03 11:54:37 +0800

    > compile NEMU ysyx_22041098 满泽远 Linux ad-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:54:37 up 4 days,  2:43,  1 user,  load average: 0.21, 0.34, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,8 +5,9 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint64_t data = 0;//((uint64_t)inl(RTC_ADDR + 4)) << 32;
-  data = data | inl(RTC_ADDR + 4);
+  uint64_t data = inl(RTC_ADDR + 4);
+  data = data << 32;
+  data = data | inl(RTC_ADDR);
   uptime->us = data;
 }
 
