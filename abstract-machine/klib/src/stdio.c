53ec42e992320f8db1e271882c327385318e7d65
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-05 11:11:23 +0800

    > compile NEMU ysyx_22041098 满泽远 Linux ad-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:11:23 up 4 days,  9:00,  1 user,  load average: 0.82, 0.52, 0.42

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -16,8 +16,9 @@ typedef struct {
 static int fmtl2str(char *out, const char *fmtl, va_list ap) {
   // char *fmtl = **fmt;
   int fmt_len = 0;
-  char buf[65];
-  memset(buf, 0, 65);
+  // int out_len = 0;
+  // char buf[65];
+  // memset(buf, 0, 65);
   char *s;
   int d;
 
@@ -45,31 +46,34 @@ static int fmtl2str(char *out, const char *fmtl, va_list ap) {
     break;
   case 'd':
     d = va_arg(ap, int);
-    char *d_buf = buf;
+    // char *d_buf = buf;
 
     // int neg = 0;
-    if (d < 0) { *d_buf = '-'; d_buf++; }
+    if (d < 0) {
+      *out = '-';
+      out++;
+    }
 
     int d_len = 1;
     int d_var = d/10;
     while (d_var != 0) { d_var /= 10; d_len ++; }
     
     for (int i = 0; i < (l.width - d_len); i ++) {
-      *d_buf = l.flags;
-      d_buf ++;
+      *out = l.flags;
+      out ++;
     }
 
     for (int i = 0 ;i < d_len; i++) {
-      d_buf[d_len - 1 - i] = d%10+'0';
+      out[d_len - 1 - i] = d%10+'0';
       d /= 10;
     }
-    strcpy(out, buf);
     out += d_len;
     break;
   default:
     assert(0);
     break;
   }
+  *out = '\0';
   fmtl ++;
   fmt_len ++;
   return fmt_len;
@@ -77,11 +81,7 @@ static int fmtl2str(char *out, const char *fmtl, va_list ap) {
 
 int printf(const char *fmt, ...) {
   char out[MAX_STR_LEN];
-  // int n = sprintf(out, fmt);
-  // for (int i = 0; i < 256 && out[i] != '\0'; i ++) {
-  //   putch(out[i]);
-  // }
-  // return n;
+  
   va_list ap;
   va_start(ap, fmt);
   int ret = vsprintf(out, fmt, ap);
@@ -94,12 +94,16 @@ int printf(const char *fmt, ...) {
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   int i = MAX_STR_LEN;
+  char buf[MAX_STR_LEN];
+  // int fmt_len = 0;
   // const char *fmtl = fmt;
 
   while (*fmt != '\0' && i --) {
     if (*fmt == '%') {
       fmt++;
-      fmt += fmtl2str(out, fmt, ap);
+      fmt += fmtl2str(buf, fmt, ap);
+      strcpy(out, buf);
+      out += strlen(buf);
     }
     strncpy(out, fmt, 1);
     out ++;
