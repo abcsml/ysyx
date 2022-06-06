5c6b5193f5adf93e6673ae629de81db8103dec91
22041098-Man Zeyuan <abcs.ml@hotmail.com>
2022-06-06 12:37:45 +0800

    pa2 over

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,18 +3,35 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+static uint32_t w;
+static uint32_t h;
+
 void __am_gpu_init() {
+  // int i;
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  w = inw(VGACTL_ADDR + 2);
+  h = inw(VGACTL_ADDR);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = w, .height = h,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *p = ctl->pixels;
+  for (uint32_t i = ctl->y; i < ctl->y + ctl->h; i ++) {
+    for (uint32_t j = ctl->x; j < ctl-> x + ctl->w; j ++) {
+      outl(FB_ADDR + i*4*w + j*4, *p);
+      // outl(FB_ADDR, *p);
+      p ++;
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
