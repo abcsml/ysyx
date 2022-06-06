01bbad39e2263746fb412e68a4cfe37609c07352
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-06 10:37:49 +0800

    > compile NEMU ysyx_22041098 满泽远 Linux ad-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:37:49 up 4 days, 14:41,  1 user,  load average: 0.29, 0.28, 0.23

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,5 +6,5 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t data = inl(KBD_ADDR);
   kbd->keydown = data < KEYDOWN_MASK ? 0 : 1;
-  kbd->keycode = data | ~KEYDOWN_MASK;
+  kbd->keycode = data < KEYDOWN_MASK ? data : data - KEYDOWN_MASK;
 }
